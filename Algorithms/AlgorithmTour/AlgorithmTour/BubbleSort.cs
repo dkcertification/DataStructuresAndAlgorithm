a83a864ae159c6fd218e875eaf34cd7604073ca2
Dilip Kurup <dkcertification@gmail.com>
2020-11-19 02:15:45 +0530

    Bubble Sort Algo

---
+++ Algorithms/AlgorithmTour/AlgorithmTour/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmTour
+{
+    class BubbleSort
+    {
+        public void Sort(int[] num)
+        {
+
+            Console.WriteLine("Input Array : " + string.Join(" ", num));
+            Console.WriteLine("========================================");
+            int Iteration = 0;
+            for (int i=0; i< num.Length-1; i++)
+            {
+                for(int j=0; j< num.Length-i-1; j++)
+                {
+                    if(num[j] > num[j + 1])
+                    {
+                        int Temp = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = Temp;
+
+                    }
+                    Console.WriteLine("-- Sorted : " + string.Join(" ", num));
+
+                }
+                Console.WriteLine("Pass [" + Iteration++ + "] Sorted : " + string.Join(" ", num));
+            }
+            Console.WriteLine("========================================");
+            Console.WriteLine("Sorted : " + string.Join(" ", num));
+        }
+    }
+}
---
+++ Algorithms/AlgorithmTour/AlgorithmTour/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort _BS = new BubbleSort();
+            int[] values = { 7, 4, 5, 2 };
+            _BS.Sort(values);
+        }
+    }
+}
