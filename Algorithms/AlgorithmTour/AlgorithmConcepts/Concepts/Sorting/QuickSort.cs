9050d42c3584748ed7f8c48008e75104e427fec7
Dilip Kurup <dkphonetic@outlook.com>
2020-12-26 16:29:08 +0530

    Clean Up

---
+++ Algorithms/AlgorithmTour/AlgorithmConcepts/Concepts/Sorting/QuickSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmConcepts.Concepts
+{
+    /*
+      Divide and Conquer
+
+     */
+    class QuickSort
+    {
+    }
+}
